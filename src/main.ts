deea3b6c32103835e5792dd0b2cdded321ce3812
awttt <350944364@qq.com>
2022-11-06 22:58:03 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import { TransformInterceptor } from './core/interceptor/transform.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('api'); // 设置全局路由前缀
+  app.useGlobalInterceptors(new TransformInterceptor());
+  await app.listen(3000);
+}
+bootstrap();
